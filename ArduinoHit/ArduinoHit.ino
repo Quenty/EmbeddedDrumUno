25e361d0cb9bd9ae341195bd63b78a30a37b8d1c
Lee Fitchett <lfitchett@gmail.com>
2017-04-04 08:29:41 -0500

    brok

--- ArduinoHit/ArduinoHit.ino
+++ ArduinoHit/ArduinoHit.ino
@@ -25,42 +25,56 @@ void setup() {
 }
 
 void loop() { 
+  Serial.write("GO");
+  go(patterns[0], lengths[0]);
+  
   // check for incoming serial data:
-  if (Serial.available() > 0) {
+  /*if (Serial.available() > 0) {
     // read incoming serial data:
     char inChar = Serial.read();
-   
-    if (inChar >= '1' && inChar <= '4'){
-        int inNum = inChar - '1';  //convert char to int (minus 1 for 0 array index)
-        go(patterns[inNum], lengths[inNum]);         
+    int inNum = (int)(inChar) - 49;
+
+    if (inNum >= 0 && inNum < 4){
+        //convert char to int (minus 1 for 0 array index)
+        inNum = 0;
+        go(patterns[inNum], lengths[inNum]);
     }
-  }
-}
+  }*/
+} 
 
 void go(const int* pattern, const int len) {
+  if (len <= 0)
+  {
+    Serial.write("Bad length");
+  }
   
   for (int i=0; i<len; i+=3) {
     int stick = pattern[i];
     int dir = pattern[i+1];
-    hit(stick, dir);
+    Serial.write("call hit");
+    hit(stick, dir); 
     delay(pattern[i+2]);
-  }
-}
+  } 
+} 
 
-void hit(int stick, int dir) {
-
+void hit(int stick, int dir) { 
+  char buffer[100];
+  sprintf(buffer, "stick %d direction %d\n", stick, dir);
+  Serial.write(buffer);
   if (stick==1) {
     Serial.write("stick1 moving\n");
     
-    digitalWrite(IN1, dir);
+    digitalWrite(IN1, dir);   
     digitalWrite(IN2, !dir);
-  }
-
-  if (stick==2) {
-    Serial.write("stick2 moving\n");
+  } else if (stick==2) {
+    Serial.write("stick2 moving\n"); 
     
     digitalWrite(IN3, dir);
     digitalWrite(IN4, !dir);
+  }
+  else
+  {
+    Serial.write("BAD INPUT\n");
   }
 }
 
