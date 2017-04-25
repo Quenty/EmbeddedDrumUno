26993c3e04e252f4cdeebbec1e1650cec16099f2
Lee Fitchett <lfitchett@gmail.com>
2017-04-24 23:00:38 -0500

    Revert "Watchdog"
    This reverts commit 107592cc363969815492c96d1ee592b61a6c3423.

--- DumbSlave/DumbSlave.ino
+++ DumbSlave/DumbSlave.ino
@@ -9,17 +9,6 @@ void setup() {
   pinMode(3, INPUT);
 
 
-  //Watchdog
-  //Disable all interrupts
-  cli();
-  //kick watchdog
-  wdt_reset();
-  //enable change on the watchdog
-  WDTCSR |= (1 << WDCE) | (1<<WDE);
-  //config the watchdog with timeout of 
-  WDTCSR = (0 <<WDIE) | (1<<WDE) | (0<<WDP3) | (1<<WDP2) | (1<<WDP1) | (0<<WDP0);
-  //enable all interrupts again
-  sei();
 }
 
 void loop() {
@@ -38,6 +27,4 @@ void loop() {
     digitalWrite(9, HIGH);   
     digitalWrite(10, LOW);
   }
-
-    wdt_reset();
 }
