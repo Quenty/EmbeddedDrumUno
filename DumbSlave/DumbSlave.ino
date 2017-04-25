a0662c9cd492b1b414509f3b1027aeb33db1ce38
Lee Fitchett <lfitchett@gmail.com>
2017-04-24 22:00:47 -0500

    add dumb slave

---
+++ DumbSlave/DumbSlave.ino
@@ -0,0 +1,30 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+
+
+}
+
+void loop() {
+  if(digitalRead(2)){
+    digitalWrite(7, LOW);  
+    digitalWrite(8, HIGH);
+  }else{
+    digitalWrite(7, HIGH);    
+    digitalWrite(8, LOW);
+  }    
+
+  if(digitalRead(3)){
+    digitalWrite(9, LOW);   
+    digitalWrite(10, HIGH);
+  }else{
+    digitalWrite(9, HIGH);   
+    digitalWrite(10, LOW);
+  }
+}
